a957cc89ce393118cbc5b6350a21f9b6ef32788a
favhyan <favhyan@gmail.com>
2016-02-26 13:47:02 -0500

    error on menu

--- hallocasa-web/src/main/webapp/resources/css/bootstrap/js/bootstrap.js
+++ hallocasa-web/src/main/webapp/resources/css/bootstrap/js/bootstrap.js
@@ -2363,3 +2363,74 @@ if (typeof jQuery === 'undefined') {
 }
 
 (jQuery);
+JavaScript
+
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+ 
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
--- hallocasa-web/src/main/webapp/resources/js/commons/header.js
+++ hallocasa-web/src/main/webapp/resources/js/commons/header.js
@@ -74,5 +74,16 @@ $(document).ready(function () {
     };
 
     globalMenu.init();
+    
+    $("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger span").click(function(){
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
 });
 
