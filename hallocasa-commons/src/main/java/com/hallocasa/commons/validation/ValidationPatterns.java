942f46156e8d586fa8aa2c5d369e3421b24cd7aa
Alexander Villamil <avillamil@lacadenaretail.com>
2016-04-26 09:33:21 -0500

    Change email pattern for accept "-" character.

--- hallocasa-commons/src/main/java/com/hallocasa/commons/validation/ValidationPatterns.java
+++ hallocasa-commons/src/main/java/com/hallocasa/commons/validation/ValidationPatterns.java
@@ -12,7 +12,14 @@ public interface ValidationPatterns {
     /**
      * Regular expression for email validation
      */
-    public static final String EMAIL_PATTERN = "(^[\\w.\\+]+@[a-zA-Z_]+?(\\.[a-zA-Z]{2,3}){1,2}$){0,1}";
+    public static final String EMAIL_PATTERN = "^(?:[a-z0-9!#$%&'*+\\/=?^_`{|}~-]"
+    		+ "+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*|(?:[\\x01-\\x08\\x0b\\x0c\\"
+    		+ "x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\[\\x01-\\x09\\x0b\\x0c\\x0e-"
+    		+ "\\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]"
+    		+ "*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}"
+    		+ "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:"
+    		+ "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\"
+    		+ "[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     /**
      * Regular expression for URL validation
      */
