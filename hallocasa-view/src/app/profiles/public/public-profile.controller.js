2edf06b00ec5601af36424d6abbe9c2ac913fa35
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2016-10-29 15:01:45 -0500

    Implementation of VirtualRepeatUtils service

--- hallocasa-view/src/app/profiles/public/public-profile.controller.js
+++ hallocasa-view/src/app/profiles/public/public-profile.controller.js
@@ -6,65 +6,16 @@
     .controller('PublicProfileController', PublicProfileController);
 
   /** @ngInject */
-  function PublicProfileController(ProfilesService, ImageValidatorService, $location, $window, $scope) {
+  function PublicProfileController(ProfilesService, ImageValidatorService, $location, $scope, VirtualRepeatUtilsService) {
     var vm = this;
     vm.validateImage = ImageValidatorService.validateBase64;
     vm.viewProfile = viewProfile;
-    vm.loadedProfiles = [];
-
+    vm.getListHeight = VirtualRepeatUtilsService.heightCalculator(".container", $scope);
+    vm.profiles = VirtualRepeatUtilsService.getVirtualRepeatInstance(ProfilesService.loadPublicProfiles, 10);
 
     function viewProfile(id) {
       $location.url("/profile");
       $location.search('id', id);
     }
-
-    vm.getListHeight = function() {
-      //TODO: ajustar calculo
-      //document.getElementsByClassName("container")[0].clientHeight
-      return {height: '' + ($window.innerHeight - 72) + 'px'};
-    };
-    $window.addEventListener('resize', onResize);
-    function onResize() {
-      $scope.$digest();
-    }
-    $scope.$on('$destroy', function() {
-      $window.removeEventListener('resize', onResize);
-    });
-
-    vm.profiles = {
-      toLoad_: 0,
-
-      // Required.
-      getItemAtIndex: function(index) {
-        if (index > vm.loadedProfiles.length) {
-          vm.profiles.fetchMoreItems_(index);
-          return null;
-        }
-
-        return vm.loadedProfiles[index];
-      },
-
-      // Required.
-      // For infinite scroll behavior, we always return a slightly higher
-      // number than the previously loaded items.
-      getLength: function() {
-        return vm.loadedProfiles.length + 1;
-      },
-
-      fetchMoreItems_: function(index) {
-        // For demo purposes, we simulate loading more items with a timed
-        // promise. In real code, this function would likely contain an
-        // $http request.
-
-        if (vm.profiles.toLoad_ < index) {
-          vm.profiles.toLoad_ += 10;
-          ProfilesService.loadPublicProfiles(vm.loadedProfiles.length, vm.loadedProfiles.length + 10)
-            .then(function (profiles) {
-              vm.loadedProfiles = _.union(vm.loadedProfiles, profiles);
-            });
-        }
-      }
-    };
-    //vm.profiles.fetchMoreItems_(10);
   }
 })();
