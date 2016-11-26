9403eec2a4758fa1c79546002bafdb868d1f76ca
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2016-11-26 16:55:55 -0500

    Property list in landing page

--- hallocasa-view/src/app/main/landing/landing.controller.js
+++ hallocasa-view/src/app/main/landing/landing.controller.js
@@ -6,13 +6,26 @@
     .controller('LandingController', LandingController);
 
   /** @ngInject */
-  function LandingController(ProfilesService) {
+  function LandingController(ProfilesService, PropertyService, ImageValidatorService, $location) {
     var vm = this;
+
+    vm.validateImage = ImageValidatorService.validateBase64;
+    vm.viewProperty = viewProperty;
 
     ProfilesService.loadPublicProfile()
       .then(function (profiles) {
         vm.profiles = profiles;
       });
 
+    PropertyService.loadProperties()
+      .then(function (properties) {
+        vm.properties = properties;
+      });
+
+    function viewProperty(id) {
+      $location.url("/property");
+      $location.search('id', id);
+    }
+    
   }
 })();
