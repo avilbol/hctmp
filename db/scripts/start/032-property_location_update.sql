3767f4eeb720693be43004192d1e41d112de8f42
Alexander <alxvilbol@gmail.com>
2017-01-29 15:14:10 -0500

    Clean name of property proposal and locations

---
+++ db/scripts/start/032-property_location_update.sql
@@ -0,0 +1,4 @@
+USE hallocasaapp;
+update property_location set name='City Center' WHERE id=1;
+update property_location set name='Suburb' WHERE id=2;
+update property_location set name='Countryside' WHERE id=3;
