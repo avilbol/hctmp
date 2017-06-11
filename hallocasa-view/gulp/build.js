4c3a300aaf610c45d6a9f33b7dda1fcac3069fd4
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-06-11 16:23:46 -0500

    Load custom safari css for fix layout-wrap issues

--- hallocasa-view/gulp/build.js
+++ hallocasa-view/gulp/build.js
@@ -96,7 +96,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js}')
+    path.join('!' + conf.paths.src, '/**/*.{html,js}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
