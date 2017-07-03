b4ef75e9f0adafe677c8d46ce1f4a14f7d7f5610
Alexander <alxvilbol@gmail.com>
2017-07-02 21:36:33 -0500

    Added automatic currency selection functionality

--- hallocasa-view/src/app/global/fields/directives/fields/currency/currency-field.directive.js
+++ hallocasa-view/src/app/global/fields/directives/fields/currency/currency-field.directive.js
@@ -11,6 +11,7 @@
       templateUrl: "app/global/fields/directives/fields/currency/currency-field.html",
       scope: {
         fieldInformation: "=",
+        additionalParameters: "=?",
         form: "=?",
         readonly: "=?"
       },
@@ -22,6 +23,34 @@
             scope.required = scope.fieldInformation.validations.includes("required");
           }
         }
+
+        function abbreviation(currencyID, options){
+          var currencyItem = _.find(options, function(option){
+            return option.id === currencyID;
+          });
+          return currencyItem.abbreviation;
+        }
+
+        function initCurrencyIDInFieldValueList(currencyID){
+          if(scope.fieldInformation.fieldValueList){
+            return;
+          }
+          scope.fieldInformation.fieldValueList = [
+            {
+              "data1": {
+                "intVal":currencyID
+              }
+            }
+          ];
+        }
+
+        function loadCurrencyID(fieldValueList){
+          var currencyID = null;
+          var existingCrcyID = fieldValueList && _.isArray(fieldValueList) && !_.isEmpty(fieldValueList);
+          var wizardCrcyID = !currencyID && scope.fieldInformation.fixCurrency && scope.additionalParameters.currencyToUse;
+          return existingCrcyID ? fieldValueList[0].data1.intVal : (wizardCrcyID ? scope.additionalParameters.currencyToUse : null);
+        }
+
         function loadCurrencyData() {
           if(scope.readonly){
             scope.$watch("fieldInformation.fieldValueList[0].data2.doubleVal", function (amount) {
@@ -36,7 +65,10 @@
           }
           FieldsService.loadOptionsByServiceId("Currency")
             .then(function (options) {
-              scope.options = options
+              scope.options = options;
+              var currencyID = loadCurrencyID(scope.fieldInformation.fieldValueList);
+              initCurrencyIDInFieldValueList(currencyID);
+              scope.abbreviation = abbreviation(currencyID, options);
             })
             .catch(function () {
               //TODO: Review message building
