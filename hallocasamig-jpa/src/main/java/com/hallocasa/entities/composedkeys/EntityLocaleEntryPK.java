da212a5945edcd96264814db0766cad31005f697
Alexander <alxvilbol@gmail.com>
2017-08-06 14:14:51 -0500

    Added equality methods to comp'osite primary keys

--- hallocasamig-jpa/src/main/java/com/hallocasa/entities/composedkeys/EntityLocaleEntryPK.java
+++ hallocasamig-jpa/src/main/java/com/hallocasa/entities/composedkeys/EntityLocaleEntryPK.java
@@ -24,4 +24,35 @@ public class EntityLocaleEntryPK {
 	public void setLocale(String locale) {
 		this.locale = locale;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((locale == null) ? 0 : locale.hashCode());
+		result = prime * result + ((pnemonic == null) ? 0 : pnemonic.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EntityLocaleEntryPK other = (EntityLocaleEntryPK) obj;
+		if (locale == null) {
+			if (other.locale != null)
+				return false;
+		} else if (!locale.equals(other.locale))
+			return false;
+		if (pnemonic == null) {
+			if (other.pnemonic != null)
+				return false;
+		} else if (!pnemonic.equals(other.pnemonic))
+			return false;
+		return true;
+	}
 }
