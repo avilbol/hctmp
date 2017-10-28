49a57922b678b1324ac23b1bf3c9c556a0dce938
Alexander <alxvilbol@gmail.com>
2017-10-28 11:44:09 -0500

    Fixing syntax error

--- hallocasa-view/src/app/main/main.controller.js
+++ hallocasa-view/src/app/main/main.controller.js
@@ -169,7 +169,7 @@
         })
         .catch(function(err){
           console.log(err);
-          CurrencyService.setCurrentCurrency({"id":3, "abbreviation":"USD"}});
+          CurrencyService.setCurrentCurrency({"id":3, "abbreviation":"USD"});
           $translate.use(LOCALES.defaultLocale);
         });
     }
