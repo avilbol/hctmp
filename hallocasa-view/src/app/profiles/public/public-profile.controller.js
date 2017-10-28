0d16812112f1750ee5a458e5d3547cbf0cb35c6c
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-10-28 01:50:41 -0500

    Initial base for public profile filters

--- hallocasa-view/src/app/profiles/public/public-profile.controller.js
+++ hallocasa-view/src/app/profiles/public/public-profile.controller.js
@@ -6,12 +6,18 @@
     .controller('PublicProfileController', PublicProfileController);
 
   /** @ngInject */
-  function PublicProfileController(ProfilesService, BrowserDetectionService) {
+  function PublicProfileController(ProfilesService, BrowserDetectionService, translateFilter, $rootScope, $scope,
+                                   $mdDialog) {
     var vm = this;
     var excludeIdList = [];
     var amountProfiles = 5;
+    var filtersDialog;
+    var selectedFilters = [];
 
     vm.fetchRangeProfiles = fetchRangeProfiles;
+    vm.openFiltersDialog = openFiltersDialog;
+    vm.closeFiltersDialog = closeFiltersDialog;
+    vm.clearFilters = clearFilters;
     vm.profiles = [];
     vm.showLoading = true;
     vm.isLoading = false;
@@ -39,6 +45,107 @@
         });
     }
 
+    function loadFilters() {
+      ProfilesService.loadProfilesFilters()
+        .then(function (filtersData) {
+          vm.filters = filtersData;
+        })
+        .catch(function () {
+          toastr.warning(
+            translateFilter("hallocasa.global.error"));
+        });
+    }
+
+    function openFiltersDialog($event) {
+      filtersDialog = $mdDialog.show({
+        contentElement: "#profileFilters",
+        targetEvent: $event,
+        clickOutsideToClose: true,
+        fullscreen: true
+      });
+      filtersDialog.finally(function () {
+        //TODO: Load profiles by filters
+      });
+    }
+
+    function listenFiltersChanges() {
+      var destroyListener = $rootScope.$on("FilterSystem:filterSelected", function (event, filterInformation) {
+        var filterIndex =  _.findIndex(selectedFilters, function (selectedFilter) {
+          return selectedFilter.propertyFilter.filter.id === filterInformation.propertyFilter.filter.id;
+        });
+
+        switch(filterInformation.propertyFilter.filter.filterType.filterTypeNature){
+          case "DROPDOWN":
+            processDropdownSelection(filterInformation, filterIndex);
+            break;
+          case "YESNO":
+            processBinarySelection(filterInformation, filterIndex);
+            break;
+          case "RANGE":
+            processRangeSelection(filterInformation, filterIndex);
+            break;
+        }
+      });
+
+      $scope.$on("$destroy", destroyListener);
+    }
+
+    function processDropdownSelection(filterInformation, filterIndex) {
+      if(_.isEmpty(filterInformation.selectedFilterOptions)){
+        selectedFilters.splice(filterIndex, 1);
+      }
+      else{
+        if(filterIndex === -1){
+          selectedFilters.push(filterInformation);
+        }
+        else{
+          selectedFilters[filterIndex] = filterInformation;
+        }
+      }
+    }
+
+    function processBinarySelection(filterInformation, filterIndex) {
+      switch (filterInformation.binaryFilterType){
+        case "Dropdown":
+          if(filterIndex === -1){
+            selectedFilters.push(filterInformation);
+          }
+          else{
+            selectedFilters[filterIndex] = filterInformation;
+          }
+          break;
+        case "Checkbox":
+          if(filterIndex === -1 && filterInformation.apply){
+            selectedFilters.push(filterInformation);
+          }
+          else{
+            selectedFilters.splice(filterIndex, 1);
+          }
+          break;
+      }
+    }
+
+    function processRangeSelection(filterInformation, filterIndex) {
+      if(filterIndex === -1){
+        selectedFilters.push(filterInformation);
+      }
+      else{
+        selectedFilters[filterIndex] = filterInformation;
+      }
+    }
+
+    function closeFiltersDialog() {
+      $mdDialog.hide();
+    }
+
+    function clearFilters() {
+      selectedFilters = [];
+      $rootScope.$broadcast("FilterSystem:clearFilters");
+    }
+
+    listenFiltersChanges();
     fetchRangeProfiles();
+    loadFilters();
+
   }
 })();
