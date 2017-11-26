80df6ed86c3679f77287a139d5165647ac21dc1b
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-11-26 18:45:12 -0500

    Redirection to user profile edit on user activation

--- hallocasa-view/src/app/session/user-activation/user-activation.controller.js
+++ hallocasa-view/src/app/session/user-activation/user-activation.controller.js
@@ -15,8 +15,8 @@
 
       SessionService.activateUser(email, activationKey)
         .then(function(){
-          toastr.success(
-            translateFilter('Alert.profileactivated'));
+          toastr.success(translateFilter('Alert.profileactivated'));
+          $location.url("/profile/my-profile/edit");
           closeDialog();
         })
         .catch(function(){
