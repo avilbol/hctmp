626968d2e664dfb76668e904cbadfe8501ef82bc
Juan Gomez <zatkiel837@gmail.com>
2017-11-27 23:14:26 -0400

    filtro perfiles limpiar codigo

--- hallocasa-view/src/app/global/filters/directives/filters/text/text-filter.directive.js
+++ hallocasa-view/src/app/global/filters/directives/filters/text/text-filter.directive.js
@@ -21,10 +21,6 @@
         scope.emitSelectedOption = emitSelectedOption;
         scope.filter = {selected: ''};
 
-        function change(){
-          console.log('Value ', scope.filter.selected);
-        }
-
         function emitSelectedOption() {
           var selectionPayload = {
             propertyFilter: scope.filterInformation,
@@ -36,25 +32,12 @@
         }
 
         function detectTextFilterType() {
-          console.log('Load text-filter');
           var filterType = scope.filterInformation.filter.filterType;
-          // if(filterType.useYesNoDropdown){
-          //   scope.textFilterType = "Dropdown";
-          //   return;
-          // }
-          // if(filterType.useCheckbox){
-          //   scope.textFilterType = "Checkbox";
-          //   return;
-          // }
-          // if(filterType.useRadio){
-          //   scope.textFilterType = "Radio";
-          //   return;
-          // }
+
           if(filterType.useText){
             scope.textFilterType = "Text";
             return;
           }
-
           //Default type
           scope.textFilterType = "Text";
         }
