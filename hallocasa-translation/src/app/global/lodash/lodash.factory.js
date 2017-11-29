21929e3a327c14a61ccf350d9e5ec2c3e5697ed6
Juan Gomez <zatkiel837@gmail.com>
2017-11-29 00:31:16 -0400

    nueva version de traducciones

---
+++ hallocasa-translation/src/app/global/lodash/lodash.factory.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('HalloCasaAdmin.global', [])
+    .factory('_', loadLodash);
+
+  function loadLodash($window) {
+    return $window._;
+  }
+})();
