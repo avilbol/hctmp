1fb44d4445eb366467d0d03aa65c9bddc37b1322
Juan Gomez <zatkiel837@gmail.com>
2017-12-07 13:59:11 -0400

    issue 155 resuelta

--- hallocasa-view/src/app/global/image-uploader/image-uploader.controller.js
+++ hallocasa-view/src/app/global/image-uploader/image-uploader.controller.js
@@ -5,23 +5,37 @@
 		.module('HalloCasa.global')
 		.controller('ImageUploaderController', ImageUploaderController);
 
-	function ImageUploaderController($mdDialog, $scope) {
+	function ImageUploaderController($mdDialog, $scope, ImageValidatorService, toastr, translateFilter) {
 		var vm = this;
 
     vm.closeDialog = closeDialog;
     vm.imageLoaded = imageLoaded;
     vm.handleFileSelect = handleFileSelect;
     vm.myCroppedImage = null;
+    vm.submitButton = true;
+
+    var isImageType = function(type,name){
+      return (type.match('image.*') || name.match(/\.(gif|png|jpe?g)$/i)) ? true : false;
+    };
 
     function handleFileSelect(event) {
       var file = event.currentTarget.files[0];
-      var reader = new FileReader();
-      reader.onload = function (evt) {
-        $scope.$apply(function (vm) {
-          vm.myImage = evt.target.result;
-        });
-      };
-      reader.readAsDataURL(file);
+      var type =  file.type;
+      var name =  file.name;
+      if(isImageType(type,name)){
+        vm.submitButton = false;
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+          $scope.$apply(function (vm) {
+            vm.myImage = evt.target.result;
+          });
+        };
+        reader.readAsDataURL(file);
+      }  else {
+        toastr.warning(
+          translateFilter("Image.Edition.Invalid.NotImage")
+        );
+      }      
     }
 
     function closeDialog(){
