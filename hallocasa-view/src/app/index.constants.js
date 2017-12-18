718633874bef7ba039b867d692e5062a2026abca
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-12-18 01:52:00 -0500

    Updated version to 1.4.24

--- hallocasa-view/src/app/index.constants.js
+++ hallocasa-view/src/app/index.constants.js
@@ -4,7 +4,7 @@
 
   angular
     .module('HalloCasa')
-    .constant('AppVersion', '1.4.23')
+    .constant('AppVersion', '1.4.24')
     .constant('INTERCOM_APPID', 't6itp8rl')
     .constant('WOOTRIC_APPID', 'NPS-ead65c4a')
     .constant('MAILCHIMP_AID', 'hallocasa.us10')
