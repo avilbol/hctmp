f40c6f493dad947b5c4bcb382eaa31d88ba57553
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2017-12-17 23:28:26 -0500

    Validation code refactor

--- hallocasa-view/src/app/global/ng-messages-generator/ng-messages-generator.directive.js
+++ hallocasa-view/src/app/global/ng-messages-generator/ng-messages-generator.directive.js
@@ -25,21 +25,6 @@
         scope.matchPassword = function () {
           return scope.validationParams.passwordToMatch.firstPassword === scope.validationParams.passwordToMatch.secondPassword;
         };
-
-        scope.dateMinorThan = function () {
-          if(!scope.validationParams.dateValidation.minorDate || !scope.validationParams.dateValidation.greaterDate){
-            return true;
-          }
-
-          var validationsParams = scope.validationParams.dateValidation;
-          var minorDate = moment(validationsParams.minorDate, validationsParams.dateFormat);
-          var greaterDate = moment(validationsParams.greaterDate, validationsParams.dateFormat);
-
-          scope.validationParams.dateValidation.greaterDateFormated = greaterDate.format(validationsParams.dateFormat);
-          var valid = minorDate.isSameOrBefore(greaterDate);
-          scope.form[scope.inputName].$setValidity("dateMinorThan", valid);
-          return valid;
-        };
       }
     };
   }
