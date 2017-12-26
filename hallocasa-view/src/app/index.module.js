df5888f66cc7c3bd57680ff13a1b583640d44d42
Juan Gomez <zatkiel837@gmail.com>
2017-12-26 12:26:09 -0400

    quitar cookie law y arreglar scroll a cabecera

--- hallocasa-view/src/app/index.module.js
+++ hallocasa-view/src/app/index.module.js
@@ -7,6 +7,8 @@
       'ngAnimate', 'ngResource', 'ngCookies', 'ngRoute', 'ngMaterial', 'toastr', 'pascalprecht.translate', 'ngSanitize',
       'tmh.dynamicLocale', 'vAccordion', 'ngMessages', 'lfNgMdFileInput', 'pdf', 'LocalStorageModule',
       'angular-spinkit', 'vcRecaptcha', 'vr.directives.slider', 'uz.mailto', '720kb.socialshare', 'ngTouch', 'ngTouchstart',
+      // Cookie Law
+      // 'angular-cookie-law',
       //App modules
       'HalloCasa.environment.constants',
       'HalloCasa.global',
